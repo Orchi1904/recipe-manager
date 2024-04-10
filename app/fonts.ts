a2bdac18fc010e61076d32e46073fca594cf45b3
Alexej Kunz <alexej1@web.de>
2024-04-10 18:22:14 +0200

    Started implementing Header and Hero

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Tilt_Neon } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: '--font-inter'
+});
+
+export const tiltNeon = Tilt_Neon({
+  subsets: ["latin"],
+  variable: '--font-tilt-neon'
+});
---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header className="bg-[#fafafa]">
+      <nav className="max-w-7xl mx-auto py-2 px-4 md:py-4">
+        <div className="flex justify-center font-bold md:justify-start">
+          <Link href="/" className="text-2xl w-fit font-tilt md:text-4xl">
+            Rezept<span className="text-lime-500">Manager</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
