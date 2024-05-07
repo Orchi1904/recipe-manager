6032ba372d94868b70baa89147181238fc845f5b
Alexej Kunz <alexej1@web.de>
2024-05-07 12:50:40 +0200

    Finished implementing Tags

--- helper/tagBuilder.tsx
+++ helper/tagBuilder.tsx
@@ -1,31 +1,77 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMugHot, faUtensils, faMoon } from "@fortawesome/free-solid-svg-icons";
-
-const tagClassName = "flex items-center gap-1";
+import {
+  faMugHot,
+  faUtensils,
+  faMoon,
+  faIceCream,
+  faMartiniGlass,
+  faWheatAwn,
+  faSlash,
+  faFish,
+  faLeaf,
+  faSeedling,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const tagBuilder: TagBuilder = {
   breakfast: (
-    <div className={tagClassName}>
-      <FontAwesomeIcon className="text-rm_detail" icon={faMugHot} />
-      Frühstück
-    </div>
+    <>
+      <FontAwesomeIcon icon={faMugHot} />
+      <span className="text-black">Frühstück</span>
+    </>
   ),
   lunch: (
-    <div className={tagClassName}>
-      <FontAwesomeIcon className="text-rm_detail" icon={faUtensils} />
-      Mittagessen
-    </div>
+    <>
+      <FontAwesomeIcon icon={faUtensils} />
+      <span className="text-black">Mittagessen</span>
+    </>
   ),
   dinner: (
-    <div className={tagClassName}>
-      <FontAwesomeIcon className="text-rm_detail" icon={faMoon} />
-      Abendessen
-    </div>
+    <>
+      <FontAwesomeIcon icon={faMoon} />
+      <span className="text-black">Abendessen</span>
+    </>
   ),
-  /* desserts: "Desserts",
-  drinks: "Getränke",
-  gluten_free: "Glutenfrei",
-  pescetarian: "Pescetarisch",
-  vegan: "Vegan",
-  vegetarian: "Vegetarisch", */
+  desserts: (
+    <>
+      <FontAwesomeIcon icon={faIceCream} />
+      <span className="text-black">Desserts</span>
+    </>
+  ),
+  drinks: (
+    <>
+      <FontAwesomeIcon icon={faMartiniGlass} />
+      <span className="text-black">Getränke</span>
+    </>
+  ),
+  gluten_free: (
+    <>
+      <span className="relative flex items-center justify-center">
+        <FontAwesomeIcon
+          className="absolute"
+          style={{ color: "#6E563F" }}
+          icon={faSlash}
+        />
+        <FontAwesomeIcon transform={{ rotate: -45 }} icon={faWheatAwn} />
+      </span>
+      <span className="text-black">Glutenfrei</span>
+    </>
+  ),
+  pescetarian: (
+    <>
+      <FontAwesomeIcon icon={faFish} />
+      <span className="text-black">Pescetarisch</span>
+    </>
+  ),
+  vegan: (
+    <>
+      <FontAwesomeIcon icon={faSeedling} />
+      <span className="text-black">Vegan</span>
+    </>
+  ),
+  vegetarian: (
+    <>
+      <FontAwesomeIcon icon={faLeaf} />
+      <span className="text-black">Vegetarisch</span>
+    </>
+  ),
 };
