f2ca7f8900e412c684bf2f820b38af688a50df36
Alexej Kunz <alexej1@web.de>
2024-05-29 18:42:44 +0200

    Started implementing Checkbox

--- components/RecipeIngredients.tsx
+++ components/RecipeIngredients.tsx
@@ -3,6 +3,9 @@
 import { useState } from "react";
 import RecipePortionSelector from "./RecipePortionSelector";
 
+/* Todo: Aussehen der Checkbox an das restliche Design anpassen
+         + Durchstreichen implementieren (evtl über beide Teile in Strich!) */
+
 function RecipeIngredients({ ingredients }: { ingredients: Ingredient[] }) {
   const [portionCount, setPortionCount] = useState(1);
 
@@ -22,6 +25,19 @@ function RecipeIngredients({ ingredients }: { ingredients: Ingredient[] }) {
               key={item._key}
             >
               <span className="col-span-1 text-end flex justify-end items-center">
+                <span className="text-start justify-center flex-1 ml-2 xsm:ml-4 sm:ml-6 md:ml-8 lg:ml-10">
+                  <input
+                    type="checkbox"
+                    id={`${item.ingredient_name}`}
+                    name={`${item.ingredient_name}`}
+                  />
+                  <label
+                    htmlFor={`${item.ingredient_name}`}
+                    className="sr-only"
+                  >
+                    Cross out {item.ingredient_name}
+                  </label>
+                </span>
                 {ingredientAmount?.toLocaleString("de-DE")} {item.unit}
               </span>
               <span className="col-span-2 flex items-center">
