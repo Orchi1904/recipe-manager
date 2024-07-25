c2969a98df6d9f55e629df1e4f04c493be147827
Alexej Kunz <alexej1@web.de>
2024-07-25 21:44:22 +0200

    feat: adds possibility to cross out ingredients

--- components/RecipeIngredients.tsx
+++ components/RecipeIngredients.tsx
@@ -3,9 +3,6 @@
 import { useState } from "react";
 import RecipePortionSelector from "./RecipePortionSelector";
 
-/* Todo: Aussehen der Checkbox an das restliche Design anpassen
-         + Durchstreichen implementieren (evtl über beide Teile in Strich!) */
-
 function RecipeIngredients({ ingredients }: { ingredients: Ingredient[] }) {
   const [portionCount, setPortionCount] = useState(1);
 
@@ -21,13 +18,14 @@ function RecipeIngredients({ ingredients }: { ingredients: Ingredient[] }) {
 
           return (
             <li
-              className="grid grid-cols-3 gap-4 py-1 odd:bg-rm_background md:gap-8 md:text-lg"
+              className="grid grid-cols-3 gap-4 py-1 odd:bg-rm_background md:gap-8 group"
               key={item._key}
             >
               <span className="col-span-1 text-end flex justify-end items-center">
                 <span className="text-start justify-center flex-1 ml-2 xsm:ml-4 sm:ml-6 md:ml-8 lg:ml-10">
                   <input
                     type="checkbox"
+                    className="accent-rm_footer hover:cursor-pointer"
                     id={`${item.ingredient_name}`}
                     name={`${item.ingredient_name}`}
                   />
@@ -38,10 +36,14 @@ function RecipeIngredients({ ingredients }: { ingredients: Ingredient[] }) {
                     Cross out {item.ingredient_name}
                   </label>
                 </span>
-                {ingredientAmount?.toLocaleString("de-DE")} {item.unit}
+                <span className="relative flex items-center group-has-[:checked]:text-gray-600 group-has-[:checked]:italic">
+                  {ingredientAmount?.toLocaleString("de-DE")} {item.unit}
+                  <span className="hidden group-has-[:checked]:block absolute border border-b-0 border-black left-0 right-0"></span>
+                </span>
               </span>
-              <span className="col-span-2 flex items-center">
+              <span className="col-span-2 flex items-center w-fit relative group-has-[:checked]:text-gray-600 group-has-[:checked]:italic">
                 {item.ingredient_name}
+                <span className="hidden group-has-[:checked]:block absolute border border-b-0 border-black -left-4 md:-left-8 right-0"></span>
               </span>
             </li>
           );
